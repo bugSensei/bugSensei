2a91d854bc7d29a091c060b765f90924163fd6f4
all-coder <niranjanmagesh@gmail.com>
2025-01-18 16:48:36 +0530

    added web_search()

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,17 @@
-#web search request
+import os
+import json
+import streamlit as st
+
+from utils import eurus
+# unsummarized files under /content/output/summarize -- refer utils/snowflake_agent.py
+def web_search(search_query):
+    try:
+        wt = eurus.Eurus()
+        wt.get_extracted_results(search_query)   
+    except Exception as e:
+        print("Web Extraction Failed !",e)
+
+
 
 #snowflake retrieval
 
