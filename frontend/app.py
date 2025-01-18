d36cfbdf7d9650c52e105ae936882b7cc2c25ac4
all-coder <niranjanmagesh@gmail.com>
2025-01-19 01:31:10 +0530

    updated UI

--- frontend/app.py
+++ frontend/app.py
@@ -3,49 +3,80 @@ import streamlit as st
 from mistralai import Mistral
 
 st.set_page_config(page_title="BugSensei", layout="wide")
-# client = Mistral(api_key=st.secrets['MISTRAL_API_KEY'])
-# def run_mistral(user_message, model="mistral-large-latest"):
-#     messages = [{"role": "user", "content": user_message}]
-#     chat_response = client.chat.complete(model=model, messages=messages)
-#     return chat_response.choices[0].message.content
+client = Mistral(api_key=st.secrets['MISTRAL_API_KEY'])
+
+def run_mistral(user_message, model="mistral-large-latest"):
+    messages = [{"role": "user", "content": user_message}]
+    chat_response = client.chat.complete(model=model, messages=messages)
+    return chat_response.choices[0].message.content
 
 
 st.markdown(
     """
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Roboto+Flex:opsz,wght@8..144,100..1000&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Roboto+Flex:opsz,wght@8..144,100..1000&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+
+[data-testid="stAppViewContainer"]{
+background-color:#f8f9fa;
+}
+[data-testid="stHeader"]{
+background-color:#f8f9fa;
+}
+[data-testid="stTool"]{
+background-color:#f8f9fa;
+}
+[data-testid="stBottomBlockContainer"]{
+background-color:#f8f9fa;
+}
+
+.stChatInput{
+margin-right:100px;
+background-color:white;
+
+}
+.st-be{
+color:black;
+}
+
+.st-bw{
+caret-color:black;
+}
 
 .heading {
     display: flex;
     justify-content: center;
     align-items: center;
     font-size: 50px; /* Increase font size */
-    font-family: "Noto Sans", sans-serif; /* Apply imported font */
-    font-weight: 700; /* Adjust font weight if needed */
-    color: #fffcf2; /* Optional: Change text color */
+    font-family: "IBM Plex Mono", sans-serif; /* Apply imported font */
+    font-weight: 300; /* Adjust font weight if needed */
+    color: black; /* Optional: Change text color */
     margin: 20px 0; /* Optional: Add spacing */
 }
 
 .chat-container {
     display: flex;
     flex-direction: column;
+    align-items:center;
     width: 100%;
     background-color: #403d39;
 }
 
 .user-message, .assistant-message {
-    border-radius: 20px;
-    border: 2px solid white; /* Add a white outline */
+    border-radius: 10px;
+    /*border: 2px solid white; Add a white outline */
     background-color: transparent; /* Transparent background */
-    padding: 15px;
+    padding-left: 30px;
+    padding-right:30px;
+    padding-top:10px;
+    padding-bottom:10px;
     margin-bottom: 15px;
     max-width: 70%;
     width: fit-content;
     word-wrap: break-word;
     overflow-wrap: break-word;
-    color: white; /* White text color for visibility */
+    color: #232323; /* White text color for visibility */
     font-size: 18px;
-    font-family: "Noto Sans", serif;
+    font-family: "IBM Plex Mono", serif;
     font-weight: 400;
     font-style: normal;
 }
@@ -54,12 +85,14 @@ st.markdown(
     align-self: flex-end;
     margin-left: auto;
     text-align: left;
+    background-color:#dee2e6
 }
 
 .assistant-message {
-    align-self: flex-start;
-    margin-right: auto;
+    align-self: center;
+    margin-right: 0 auto;
     text-align: left;
+    background-color:#dee2e6
 }
 
 </style>
@@ -94,8 +127,9 @@ if prompt := st.chat_input("Type your message here..."):
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.markdown(f'<div class="user-message">{prompt}</div>', unsafe_allow_html=True)
 
-    # Uncomment and modify the following when Mistral API is set up
-    # with st.spinner("Let me see..."):
-    #     full_response = run_mistral(prompt)
-    # st.session_state.messages.append({"role": "assistant", "content": full_response})
-    # st.markdown(f'<div class="assistant-message">{full_response}</div>', unsafe_allow_html=True)
+    with st.spinner():
+        full_response = run_mistral(prompt)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
+    st.markdown(
+        f'<div class="assistant-message">{full_response}</div>', unsafe_allow_html=True
+    )
