e9756101f2847ed7f00677fb2e13e36324260813
all-coder <niranjanmagesh@gmail.com>
2025-01-20 21:47:08 +0530

    modified eurus.py to check if it is working

--- frontend/app.py
+++ frontend/app.py
@@ -2,17 +2,20 @@ import os
 import streamlit as st
 from mistralai import Mistral
 import sys
+import json
+
+from utils import eurus
 
 
 sys.path.append('.') # necessary for importing files
 
-st.set_page_config(page_title="BugSensei", layout="wide")
-client = Mistral(api_key=st.secrets['MISTRAL_API_KEY'])
+# st.set_page_config(page_title="BugSensei", layout="wide")
+# client = Mistral(api_key=st.secrets['MISTRAL_API_KEY'])
 
-def run_mistral(user_message, model="mistral-large-latest"):
-    messages = [{"role": "user", "content": user_message}]
-    chat_response = client.chat.complete(model=model, messages=messages)
-    return chat_response.choices[0].message.content
+# def run_mistral(user_message, model="mistral-large-latest"):
+#     messages = [{"role": "user", "content": user_message}]
+#     chat_response = client.chat.complete(model=model, messages=messages)
+#     return chat_response.choices[0].message.content
 
 
 st.markdown(
@@ -104,36 +107,62 @@ caret-color:black;
     unsafe_allow_html=True,
 )
 
-st.markdown('<div class="heading">BugSensei</div>', unsafe_allow_html=True)
+# st.markdown('<div class="heading">BugSensei</div>', unsafe_allow_html=True)
 
-if "messages" not in st.session_state:
-    st.session_state.messages = [
-        {"role": "assistant", "content": "Let's fix your computer, shall we?"}
-    ]
+# if "messages" not in st.session_state:
+#     st.session_state.messages = [
+#         {"role": "assistant", "content": "Let's fix your computer, shall we?"}
+#     ]
 
-st.markdown('<div class="chat-container">', unsafe_allow_html=True)
+# st.markdown('<div class="chat-container">', unsafe_allow_html=True)
 
-for message in st.session_state.messages:
-    if message["role"] == "user":
-        st.markdown(
-            f'<div class="user-message">{message["content"]}</div>',
-            unsafe_allow_html=True,
-        )
-    else:
-        st.markdown(
-            f'<div class="assistant-message">{message["content"]}</div>',
-            unsafe_allow_html=True,
-        )
+# for message in st.session_state.messages:
+#     if message["role"] == "user":
+#         st.markdown(
+#             f'<div class="user-message">{message["content"]}</div>',
+#             unsafe_allow_html=True,
+#         )
+#     else:
+#         st.markdown(
+#             f'<div class="assistant-message">{message["content"]}</div>',
+#             unsafe_allow_html=True,
+#         )
 
-st.markdown("</div>", unsafe_allow_html=True)
+# st.markdown("</div>", unsafe_allow_html=True)
 
-if prompt := st.chat_input("Type your message here..."):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.markdown(f'<div class="user-message">{prompt}</div>', unsafe_allow_html=True)
+# if prompt := st.chat_input("Type your message here..."):
+#     st.session_state.messages.append({"role": "user", "content": prompt})
+#     st.markdown(f'<div class="user-message">{prompt}</div>', unsafe_allow_html=True)
 
-    with st.spinner():
-        full_response = run_mistral(prompt)
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
-    st.markdown(
-        f'<div class="assistant-message">{full_response}</div>', unsafe_allow_html=True
-    )
+#     with st.spinner():
+#         full_response = run_mistral(prompt)
+#     st.session_state.messages.append({"role": "assistant", "content": full_response})
+#     st.markdown(
+#         f'<div class="assistant-message">{full_response}</div>', unsafe_allow_html=True
+#     )
+
+
+def main():
+    st.title("Checking Eurus")
+    ws = eurus.Eurus()
+    query = st.text_input("Enter the google search")
+
+    if st.button("Get Data"):
+        try:
+            ws.get_extracted_results(query)
+            for root, dirs, files in os.walk("./content/output"):
+                for file in files:
+                    if file.endswith(".json"):
+                        file_path = os.path.join(root, file)
+                        
+                        # Load and display the JSON content
+                        with open(file_path, "r") as f:
+                            try:
+                                json_content = json.load(f)
+                                st.subheader(f"Content of {file}:")
+                                st.json(json_content)
+                            except json.JSONDecodeError:
+                                st.error(f"Error decoding JSON in {file}")
+        except Exception as e:
+            st.text(e)
+
