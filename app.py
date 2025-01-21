877f74304cb0fa34ded2e771837e1faf52043791
all-coder <niranjanmagesh@gmail.com>
2025-01-21 23:54:11 +0530

    final changes

--- app.py
+++ app.py
@@ -18,140 +18,119 @@ from utils.snowflake_agent import Snowflake
 
 sys.path.append(".")  # necessary for importing files
 
-# st.set_page_config(page_title="BugSensei", layout="wide")
-# client = Mistral(api_key=st.secrets['MISTRAL_API_KEY'])
+st.markdown(
+    """
+<style>
+@import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Roboto+Flex:opsz,wght@8..144,100..1000&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
 
-# def run_mistral(user_message, model="mistral-large-latest"):
-#     messages = [{"role": "user", "content": user_message}]
-#     chat_response = client.chat.complete(model=model, messages=messages)
-#     return chat_response.choices[0].message.content
+[data-testid="stAppViewContainer"]{
+background-color:#f8f9fa;
+}
+[data-testid="stHeader"]{
+background-color:#f8f9fa;
+}
+[data-testid="stTool"]{
+background-color:#f8f9fa;
+}
+[data-testid="stBottomBlockContainer"]{
+background-color:#f8f9fa;
+}
 
+.stChatInput{
+margin-right:100px;
+background-color:white;
 
-# st.markdown(
-#     """
-# <style>
-# @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Roboto+Flex:opsz,wght@8..144,100..1000&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+}
+.st-be{
+color:black;
+}
 
-# [data-testid="stAppViewContainer"]{
-# background-color:#f8f9fa;
-# }
-# [data-testid="stHeader"]{
-# background-color:#f8f9fa;
-# }
-# [data-testid="stTool"]{
-# background-color:#f8f9fa;
-# }
-# [data-testid="stBottomBlockContainer"]{
-# background-color:#f8f9fa;
-# }
+.st-bw{
+caret-color:black;
+}
 
-# .stChatInput{
-# margin-right:100px;
-# background-color:white;
+.heading {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 50px; /* Increase font size */
+    font-family: "IBM Plex Mono", sans-serif; /* Apply imported font */
+    font-weight: 300; /* Adjust font weight if needed */
+    color: black; /* Optional: Change text color */
+    margin: 20px 0; /* Optional: Add spacing */
+}
 
-# }
-# .st-be{
-# color:black;
-# }
+.chat-container {
+    display: flex;
+    flex-direction: column;
+    align-items:center;
+    width: 100%;
+    background-color: #403d39;
+}
 
-# .st-bw{
-# caret-color:black;
-# }
+.user-message, .assistant-message {
+    border-radius: 10px;
+    /*border: 2px solid white; Add a white outline */
+    background-color: transparent; /* Transparent background */
+    padding-left: 30px;
+    padding-right:30px;
+    padding-top:10px;
+    padding-bottom:10px;
+    margin-bottom: 15px;
+    max-width: 70%;
+    width: fit-content;
+    word-wrap: break-word;
+    overflow-wrap: break-word;
+    color: #232323; /* White text color for visibility */
+    font-size: 18px;
+    font-family: "IBM Plex Mono", serif;
+    font-weight: 400;
+    font-style: normal;
+}
 
-# .heading {
-#     display: flex;
-#     justify-content: center;
-#     align-items: center;
-#     font-size: 50px; /* Increase font size */
-#     font-family: "IBM Plex Mono", sans-serif; /* Apply imported font */
-#     font-weight: 300; /* Adjust font weight if needed */
-#     color: black; /* Optional: Change text color */
-#     margin: 20px 0; /* Optional: Add spacing */
-# }
+.user-message {
+    align-self: flex-end;
+    margin-left: auto;
+    text-align: left;
+    background-color:#dee2e6
+}
 
-# .chat-container {
-#     display: flex;
-#     flex-direction: column;
-#     align-items:center;
-#     width: 100%;
-#     background-color: #403d39;
-# }
+.assistant-message {
+    align-self: center;
+    margin-right: 0 auto;
+    text-align: left;
+    background-color:#dee2e6
+}
 
-# .user-message, .assistant-message {
-#     border-radius: 10px;
-#     /*border: 2px solid white; Add a white outline */
-#     background-color: transparent; /* Transparent background */
-#     padding-left: 30px;
-#     padding-right:30px;
-#     padding-top:10px;
-#     padding-bottom:10px;
-#     margin-bottom: 15px;
-#     max-width: 70%;
-#     width: fit-content;
-#     word-wrap: break-word;
-#     overflow-wrap: break-word;
-#     color: #232323; /* White text color for visibility */
-#     font-size: 18px;
-#     font-family: "IBM Plex Mono", serif;
-#     font-weight: 400;
-#     font-style: normal;
-# }
+</style>
+""",
+    unsafe_allow_html=True,
+)
 
-# .user-message {
-#     align-self: flex-end;
-#     margin-left: auto;
-#     text-align: left;
-#     background-color:#dee2e6
-# }
+st.markdown('<div class="heading">BugSensei</div>', unsafe_allow_html=True)
 
-# .assistant-message {
-#     align-self: center;
-#     margin-right: 0 auto;
-#     text-align: left;
-#     background-color:#dee2e6
-# }
+if "messages" not in st.session_state:
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Let's fix your computer, shall we?"}
+    ]
 
-# </style>
-# """,
-#     unsafe_allow_html=True,
-# )
+st.markdown('<div class="chat-container">', unsafe_allow_html=True)
 
-# st.markdown('<div class="heading">BugSensei</div>', unsafe_allow_html=True)
+for message in st.session_state.messages:
+    if message["role"] == "user":
+        st.markdown(
+            f'<div class="user-message">{message["content"]}</div>',
+            unsafe_allow_html=True,
+        )
+    else:
+        st.markdown(
+            f'<div class="assistant-message">{message["content"]}</div>',
+            unsafe_allow_html=True,
+        )
 
-# if "messages" not in st.session_state:
-#     st.session_state.messages = [
-#         {"role": "assistant", "content": "Let's fix your computer, shall we?"}
-#     ]
+st.markdown("</div>", unsafe_allow_html=True)
 
-# st.markdown('<div class="chat-container">', unsafe_allow_html=True)
-
-# for message in st.session_state.messages:
-#     if message["role"] == "user":
-#         st.markdown(
-#             f'<div class="user-message">{message["content"]}</div>',
-#             unsafe_allow_html=True,
-#         )
-#     else:
-#         st.markdown(
-#             f'<div class="assistant-message">{message["content"]}</div>',
-#             unsafe_allow_html=True,
-#         )
-
-# st.markdown("</div>", unsafe_allow_html=True)
-
-# if prompt := st.chat_input("Type your message here..."):
-#     st.session_state.messages.append({"role": "user", "content": prompt})
-#     st.markdown(f'<div class="user-message">{prompt}</div>', unsafe_allow_html=True)
-
-#     with st.spinner():
-#         full_response = run_mistral(prompt)
-#     st.session_state.messages.append({"role": "assistant", "content": full_response})
-#     st.markdown(
-#         f'<div class="assistant-message">{full_response}</div>', unsafe_allow_html=True
-#     )
-
-# working commit - finally ##
-    # does not have a trailing slash at the end
+# does not have a trailing slash at the end
 def generate_temp_dir(): 
     if "session_id" not in st.session_state:
         st.session_state.session_id = str(uuid.uuid4())
@@ -162,35 +141,24 @@ def generate_temp_dir():
     # st.session_state.temp_dir = temp_path
     st.text(f"Temporary ID:{temp_path}")
     return temp_path
-# def destroy_temp_dir():
-#     if "temp_dir" in st.session_state and os.path.exists(st.session_state.temp_dir):
-#         shutil.rmtree(st.session_state.temp_dir)
-# st.session_state.on_session_end
-    # Your RedditRetriever setup
-    # reddit_retriever = RedditRetriever(
-    #     username=st.secrets["REDDIT_USERNAME"],
-    #     secret_key=st.secrets["REDDIT_SECRET_KEY"],
-    #     password=st.secrets["REDDIT_PASSWORD"],
-    #     client_id=st.secrets["REDDIT_CLIENT_ID"],
-    #     output_directory=temp_path,
-    # )
-    # stackexchange = StackExchangeRetriever(access_token=st.secrets['STACK_EXCHANGE_ACCESS_TOKEN'],secret_key=st.secrets['STACK_EXCHANGE_SECRET_KEY'])
-def main():
-    st.title("Testing Model")
+
+
+def generate_response(query):
+    # st.title("Testing Model")
     temp_path = generate_temp_dir()
     eurus = Eurus(output_directory=temp_path)
     snowflake = Snowflake()
 
-    query = st.text_input("Enter the input", "")
+    # query = st.text_input("Enter the input", "")
 
     #mc = Eurus(output_directory=temp_path)
     if st.button("Get Data"):
             try:
-                st.text("rag")
+                # st.text("rag")
                 rag_retreival = snowflake_retrieval(snowflake,query)
-                st.text(rag_retreival)
+                # st.text(rag_retreival)
                 # web results are exeucted
-                st.text("web search")
+                # st.text("web search")
                 eurus.get_extracted_results(query)
                 root_directory = temp_path+"/"
                 # Walk through the directory
@@ -210,10 +178,10 @@ def main():
                                         st.text(content)
                             except Exception as e:
                                 st.error(f"Error reading file {file_path}: {e}")
-                st.text(text_file_paths)
+                # st.text(text_file_paths)
                 print(text_file_paths)
                 snowflake.summarise(file_paths=text_file_paths,temp_path=temp_path)
-                st.text("summarized texts")
+                # st.text("summarized texts")
                 summarize_folder_path = f"{temp_path}/summarize"
                 for filename in os.listdir(summarize_folder_path):
                     file_path = os.path.join(summarize_folder_path, filename)
@@ -223,19 +191,32 @@ def main():
                             content = file.read()
                             with st.expander(f"File:{file_path}"):
                                 st.text(content)
-                st.text("ranked documents")
+                # st.text("ranked documents")
                 with open(f"{summarize_folder_path}/rag.txt","w") as f:
                     f.write(rag_retreival)
+                f.close()
+                # st.text("Going to rank documents")
                 reranked_file_path,result = rank_documents(snowflake=snowflake,query=query,summarize_folder_path=summarize_folder_path,temp_path=temp_path)
-                st.text(result)
-                st.text("Final Responses")
+                # st.text(result)
+                # st.text("Final Responses")
                 final_responses = get_user_friendly_responses(snowflake,result[0:2])
-                for i in final_responses:
-                    st.text(i)
+                # for i in final_responses:
+                #     st.text(i)
                 upload_files_to_snowflake(snowflake=snowflake,texts=result)
-                st.text("files uploaded to snowflake")
+                return final_responses
+                # st.text("files uploaded to snowflake")
             except Exception as e:
                 st.text(e)
 
+if prompt := st.chat_input("What is your issue?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.markdown(f'<div class="user-message">{prompt}</div>', unsafe_allow_html=True)
 
-main()
+    with st.spinner():
+        full_response = generate_response(prompt)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
+    for i in full_response:
+        st.markdown(
+            f'<div class="assistant-message">{i}</div>', unsafe_allow_html=True
+        )
+
